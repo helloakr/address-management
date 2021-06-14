6b2b6c622c1921b50d6bda3ac22f5d3fbe393e99
helloAkr <1405088661@qq.com>
2021-06-15 00:56:42 +0800

    complete the funtion of reading studen table

--- mian.go
+++ mian.go
@@ -3,6 +3,7 @@ package main
 import (
 	"hummingbird/middlewares"
 	"hummingbird/router"
+	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
@@ -11,6 +12,7 @@ func main() {
 	r := gin.Default()
 	r.Use(middlewares.Cors())
 	r.Use(middlewares.Session("topgoer"))
+	r.StaticFS("/static", http.Dir("./studentFile"))
 	r = router.ControlRouter(r)
 	r.Run(":8000")
 }
